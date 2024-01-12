2bebaee3890c2e051b77d275c42151e33f1f8463
huyen1607 <137878202+huyen1607@users.noreply.github.com>
2024-01-12 14:47:38 +0900

    update animation

--- view/EnrollingDeviceView.js
+++ view/EnrollingDeviceView.js
@@ -1,43 +1,72 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Box, Button, Center, HStack, Image, Text, View, VStack} from "native-base";
-import {ImageBackground, TouchableOpacity, StyleSheet} from "react-native";
+import {ImageBackground, TouchableOpacity, StyleSheet, Animated} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import {Easing} from "react-native-reanimated";
-import {MotiView} from "moti";
+
 
 const EnrollingDeviceView = ({navigation}) => {
 
     const deviceImage = require("./images/RenstDevice.png");
     const backgroundImage = require('./images/BackgroundofRenstDevice.png');
-    
+
     const handleSearch = () => {
         console.log("검색 버튼 클릭")
         navigation.navigate("DeviceSettingScreens", {screen: "CompleteEnroll"})
     }
 
-    function WaveAnimation() {
-        return (
-            <MotiView
-                from={{
-                    opacity: 0.7,
-                    scale: 1,
-                }}
-                animate={{
-                    opacity: 0,
-                    scale: 4,
-                }}
-                transition={{
-                    type: 'timing',
-                    duration: 2000,
-                    easing: Easing.out(Easing.ease),
-                    // delay: index * 400,
-                    loop: true,
-                }}
-                // key={index}
-                style={styles.circular}
-            />
-        );
-    }
+    const animatedValue1 = new Animated.Value(0);
+    const animatedValue2 = new Animated.Value(0);
+    const animatedValue3 = new Animated.Value(0);
+
+    const fadeInOutAnimation = () => {
+        const fadeInOut1 = () =>
+            Animated.loop(
+                Animated.sequence([
+                    Animated.timing(animatedValue1, {
+                        toValue: 3,
+                        duration: 2500,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                    }),
+                    Animated.delay(1000),
+                ])
+            ).start();
+
+        const fadeInOut2 = () =>
+            Animated.loop(
+            Animated.sequence([
+                Animated.delay(500), // Adjust the delay time as needed
+                Animated.timing(animatedValue2, {
+                    toValue: 3,
+                    duration: 2500,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                }),
+                Animated.delay(500),
+            ])
+    ).start();
+
+        const fadeInOut3 = () =>
+            Animated.loop(
+                Animated.sequence([
+                    Animated.delay(1000), // Adjust the delay time as needed
+                    Animated.timing(animatedValue3, {
+                        toValue: 3,
+                        duration: 2500,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                    }),
+                ])
+            ).start();
+        fadeInOut1();
+        fadeInOut2()
+        fadeInOut3()// Start the first loop
+    };
+
+    useEffect(() => {
+        fadeInOutAnimation();
+    }, []);
 
 
     return(
@@ -51,15 +80,44 @@ const EnrollingDeviceView = ({navigation}) => {
 
         <VStack flex={1} justifyContent={"space-between"} bg={"white"}>
             <Center flex={0.72} overflow={"hidden"} py={3}>
-                    {/*<ImageBackground source={backgroundImage} resizeMode={'cover'} alt={"Background Image"}*/}
-                    {/*style={{justifyContent: "center", height: 800}}>*/}
-                    {/*    <Image source={deviceImage} alt={"Device Image"} />*/}
-                    {/*</ImageBackground>*/}
+                <Box width={"100%"} alignItems={"center"} justifyContent={"center"} style={{position:"absolute"}}>
+                    <Animated.View
+                        style={{
+                            width: 300,
+                            height: 300,
+                            borderRadius:300,
+                            borderWidth:1,
+                            borderColor:`rgba(39,122,244,0.5)`,
+                            transform: [{ scale: animatedValue1 }]
+                        }}
+                    />
+                </Box>
+                <Box width={"100%"} alignItems={"center"} justifyContent={"center"} style={{position:"absolute"}}>
+                    <Animated.View
+                        style={{
+                            width: 300,
+                            height: 300,
+                            borderRadius:300,
+                            borderWidth:1,
+                            borderColor:`rgba(39,122,244,0.5)`,
+                            transform: [{ scale: animatedValue2 }]
+                        }}
+                    />
+                </Box>
+                <Box width={"100%"} alignItems={"center"} justifyContent={"center"} style={{position:"absolute"}}>
+                    <Animated.View
+                        style={{
+                            width: 300,
+                            height: 300,
+                            borderRadius:300,
+                            borderWidth:1,
+                            borderColor:`rgba(39,122,244,0.5)`,
+                            transform: [{ scale: animatedValue3 }]
+                        }}
+                    />
+                </Box>
 
-                {/*<Box width={250} height={250} borderRadius={125} bg={"blue.300"} position={"absolute"} />*/}
-                <WaveAnimation />
-                    <Image source={deviceImage} alt={"Device Image"} position={"relative"}/>
-
+                <Image source={deviceImage} alt={"Device Image"} position={"relative"}/>
             </Center>
             <VStack flex={0.28} py={3} justifyContent={"space-between"} alignItems={"center"}>
                 <VStack justifyContent={'center'} alignItems={'center'} space={2}>
